fa44eb0f06629b0077af7a42e7ca9b0948a010fe
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2025-04-28 02:17:37 -0400

    Comments deleted

--- Week3/Lab3/Lab3/Program.cs
+++ Week3/Lab3/Lab3/Program.cs
@@ -6,20 +6,7 @@ using FullSailCasino;
 
 namespace Lab3
 {
-    //
-    //------------Lab 3 Notes-------------
-    //      Add your classes to the BlackjackClassLibrary project.
-    //      Add the menu code to the Main method.
-    //
 
-
-    //
-    //------------Lab 4 Notes-------------
-    //      This project and file will be reused for lab 4. 
-    //      When you start working on lab 4...
-    //          Add your BlackjackGame class to the FullSailCasino project.
-    //          update the menu code for Play Blackjack menu option
-    //
 
     class Program
     {
